67f27d074f097ac7bdf196a01ecd39fb6c261ae7
Tran Minh Khanh <59864163+khanhlove01@users.noreply.github.com>
2022-12-21 16:22:54 +0700

    Create Enemy.h

---
+++ Enemy.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include <string>
+class Enemy
+{
+private:
+	sf::Texture* textureEnemy;
+	sf::Sprite spriteEnemy;
+
+	void initVariables();
+	void initShapes(std::string name);
+
+public:
+
+	Enemy(std::string name);
+	virtual ~Enemy();
+
+};
