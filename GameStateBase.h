b0d0de427e2b148662be48fa21627d9502c5a8fa
pnguyenhere <phnguyen.pn001@gmail.com>
2022-12-22 08:52:16 +0700

    Update GameStateBase.h

--- GameStateBase.h
+++ GameStateBase.h
@@ -1,1 +1,22 @@
+#pragma once
+#include "Player.h"
+#include <iostream>
+#include <fstream>
 
+class GameStateBase
+{
+private:
+public:
+	GameStateBase() {};
+	virtual ~GameStateBase() {};
+
+	virtual void Exit() = 0;
+	virtual void Pause() = 0;
+	virtual void Resume() = 0;
+
+	virtual void Init() = 0;
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+
+
+};
