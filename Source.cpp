40f6e3c5954d574e4a3de12dd3471d562e6f6323
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-12-23 21:39:58 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include "GSMenu.h"
 
 int main() {
-  
+  std::srand(static_cast<unsigned>(time(NULL)));
+	sf::Music music;
+	if (!music.openFromFile("../Data/Music/music.wav"))
+		return -1; // error
+	music.play();
+	music.setLoop(true);
+	GSMenu gsmenu;
+
+	gsmenu.run();
+
   return 0;
 }
