b8c3e33ca514e366f635f36b74acf863b14ab3c5
pnguyenhere <phnguyen.pn001@gmail.com>
2022-12-26 15:17:13 +0700

    Update GSMenu.h

--- GSMenu.h
+++ GSMenu.h
@@ -2,3 +2,43 @@
 #include "Game.h"
 #include "GameStateBase.h"
 
+class GSMenu : public GameStateBase
+{
+private:
+	Game game;
+	sf::RenderWindow* window;
+	int currentState;
+	//loadgame
+	int velocityLoadGame;
+	sf::Vector2f positionPlayerLoadGame;
+	//text file
+	std::ifstream fin;
+	std::ofstream fout;
+	//mouse pos
+	sf::Vector2i mousePosWindow;
+	sf::Vector2f mousePosView;
+
+	sf::Texture textureBackground;
+	sf::Sprite spriteBackGround;
+
+	sf::Texture texture1;
+	sf::Sprite shape1;
+
+	sf::Texture texture2;
+	sf::Sprite shape2;
+
+	sf::Texture texture3;
+	sf::Sprite shape3;
+
+	sf::Texture texture4;
+	sf::Sprite shape4;
+
+	sf::Texture texture5;
+	sf::Sprite shape5;
+
+	sf::Font font;
+	sf::Text text;
+
+	sf::Text textPlay;
+	sf::Text saveGame;
+}
