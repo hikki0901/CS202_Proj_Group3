7e9f41f8c4a3c961a8c2030d4cccdd864833158a
Tran Minh Khanh <59864163+khanhlove01@users.noreply.github.com>
2023-01-03 19:28:07 +0700

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,39 +1,21 @@
 #include "Enemy.h"
 
-void Enemy::initVariables()
-{
-}
-
-void Enemy::initShapes(std::string name)
-{
+void Enemy::initShapes(std::string name) {
+	
 	this->textureEnemy = new sf::Texture();
 	this->textureEnemy->loadFromFile("../Data/Textures/" +name +".png");
 	this->spriteEnemy.setTexture(*this->textureEnemy);
-	
-	//this->spriteBat.setPosition(200, 100);
-	//this->spriteBat.scale(1.2, 1.2);
 }
 
-Enemy::Enemy(std::string name)
-{
+Enemy::Enemy(std::string name) {
 	this->initVariables();
 	this->initShapes(name);
 }
 
-Enemy::~Enemy() 
-{
-}
-
-sf::Sprite& Enemy::getSprite()
-{
+sf::Sprite& Enemy::getSprite() {
 	return this->spriteEnemy;
 }
 
-void Enemy::update()
-{
-}
-
-void Enemy::render(sf::RenderTarget* target)
-{
+void Enemy::render(sf::RenderTarget* target) {
 	target->draw(this->spriteEnemy);
 }
